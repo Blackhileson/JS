a2348e69ee46043dd5fd4fcc8fb00594aa955241
Savan Patel <71623498+Blackhileson@users.noreply.github.com>
2025-09-30 10:33:13 +0000

    let, const, var ki kahani

--- Basics/First.js
+++ Basics/First.js
@@ -1,1 +1,20 @@
 console.log("Hello Sir");
+
+const accountId = 144553
+let accountEmail = "savan@gmail.com"
+var accounrPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hehe@gmail.com"
+accounrPassword = "21212121"
+accountCity = "Bengluru"
+
+/*
+Prefer not to use Var
+becasue of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accounrPassword, accountCity]);
