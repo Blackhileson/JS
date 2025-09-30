65a17cfa0c288f637500e0bd3540e4592d8c2ac4
Savan Patel <71623498+Blackhileson@users.noreply.github.com>
2025-09-30 11:30:19 +0000

    Datatypes and ECMA

--- Basics/First.js
+++ Basics/First.js
@@ -18,3 +18,20 @@ becasue of issue in block scope and functional scope
 
 console.log(accountId);
 console.table([accountId, accountEmail, accounrPassword, accountCity]);
+
+let name = "savan"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof age);
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
