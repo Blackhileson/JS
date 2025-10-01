bf55ca93bbfc0f826693fa080b0069f62a369101
Savan Patel <71623498+Blackhileson@users.noreply.github.com>
2025-10-01 10:56:20 +0000

    conversionoperationpart1

---
+++ Basics/Conversion.js
@@ -0,0 +1,15 @@
+let hello = "Welcome"
+// let hello = "Welcome123"
+// let hello = null
+// let hello = undefined
+let test = Number(hello)
+let stringNumber = Boolean(hello)
+console.log(stringNumber);
+console.log(typeof test);
+console.log(test);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0;
