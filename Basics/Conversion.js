e63d8e73a607d1209d7d7d4d977801584ccbfcf4
Savan Patel <71623498+Blackhileson@users.noreply.github.com>
2025-10-01 11:26:35 +0000

    postfixandprefixconfusion

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -4,12 +4,62 @@ let hello = "Welcome"
 // let hello = undefined
 let test = Number(hello)
 let stringNumber = Boolean(hello)
-console.log(stringNumber);
-console.log(typeof test);
-console.log(test);
+// console.log(stringNumber);
+// console.log(typeof test);
+// console.log(test);
 
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1;
 // false => 0;
+
+// ******************************** Operations  ************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Savan"
+let str2 = "Patel"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// Use Always parentheses
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(true);
+// console.log(+false);
+// console.log(false);
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// let fValue = 3
+// const sValue = fValue++
+// console.log("x:" + fValue+" "  + "y:" + sValue);
+
+// let fValues = 3
+// const sValues = ++fValues
+// console.log("x:" + fValues+" " + "y:" + sValues);
+
+
+
+
+
+
