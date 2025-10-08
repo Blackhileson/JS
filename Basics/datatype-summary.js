558a7fc11bfd3db117d95411cdd9f45e7fe9fd49
Savan Patel <71623498+Blackhileson@users.noreply.github.com>
2025-10-08 12:29:41 +0000

    datatypesummary

---
+++ Basics/datatype-summary.js
@@ -0,0 +1,44 @@
+// Primitives
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = '100'
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol("123")
+const anotherid = Symbol("123")
+
+// console.log(id == anotherid);
+
+const bigNumber = 687865453453746874668768n
+
+// console.log(typeof bigNumber);
+
+
+
+// Reference (Non Premitive)
+
+// Array, Objects, functions
+
+const heros = ["hello","world"];
+const heross = {name:"savan",age:23}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof score);
+console.log(typeof scorevalue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof heross);
+console.log(typeof myFunction);
+
+
